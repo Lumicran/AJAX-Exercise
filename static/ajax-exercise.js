2509cb9b52b80b194f6bd482bcdf85dd33b9b8d9
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-28 13:09:16 -0700

    Complete Random Fortune & Weather

---
+++ static/ajax-exercise.js
@@ -0,0 +1,72 @@
+"use strict";
+
+
+// PART 1: SHOW A FORTUNE
+
+
+
+function showFortune(evt) {
+    // TODO: get the fortune and show it in the #fortune-text div
+
+    evt.preventDefault();
+
+    $.get('/fortune', function (results) {$('#fortune-text').html(results)}); 
+
+}
+
+
+$('#get-fortune-button').on('click', showFortune);
+
+
+
+// PART 2: SHOW WEATHER
+
+function showWeather(evt) {
+    evt.preventDefault();
+
+    let url = "/weather.json";
+    let formData = {"zipcode": $("#zipcode-field").val()};
+
+    $.get(url, formData, function (results) {
+                              $('#weather-info').html(results.forecast)});
+
+    // TODO: request weather with that URL and show the forecast in #weather-info
+}
+
+$("#weather-form").on('submit', showWeather);
+
+
+
+// PART 3: ORDER MELONS
+
+function orderMelons(evt) {
+    evt.preventDefault();
+
+    // TODO: show the result message after your form
+    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
+
+    let url = "/order-melons.json";
+    let formData = {
+                    "qty": $("#qty-field").val(),
+                    "melon_type": $("#melon-type-field").val(),
+                   };
+
+    $.post(url, formData, showOrderResult);
+}
+
+function showOrderResult(results) {
+  console.log(results);
+
+  let orderText = results.code + ": " + results.msg;
+
+  $('#order-status').html(orderText);
+
+  // if (results.result_code === "ERROR") {
+
+  // } 
+}
+
+$("#order-form").on('submit', orderMelons);
+
+
